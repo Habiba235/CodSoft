a7a2fd92da49f482dc2c7f731345f861cecd707c
Habiba235 <154216728+Habiba235@users.noreply.github.com>
2024-07-03 14:28:22 +0300

    Update Task1CodSoft.java

--- Task1CodSoft.java
+++ Task1CodSoft.java
@@ -1,6 +1,6 @@
 package task1codsoft;
 import java.util.Scanner;
-public class Task1CodSoft {
+public class Task1 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
